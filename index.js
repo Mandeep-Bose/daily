dcd8bca2324b165ef7021ab012651ec2ba83b275
Mandeep Bose <94315656+Mandeep-Bose@users.noreply.github.com>
2022-03-03 20:54:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+// const age = 25
+// switch (true) {
+//     case (age < 18):
+//         console.log("child")
+//         break;
+//     case (age < 18 < 40):
+//         console.log("adult")
+//         break;
+//     default:
+//         console.log("old")
+//         break;
+// }
+
+// const day = 4;
+// switch (day) {
+//     case 1:
+//         console.log("monday")
+//         break;
+//     case 2:
+//         console.log("tuesday")
+//         break;
+//     case 3:
+//         console.log("wednesday")
+//         break;
+//     case 4:
+//         console.log("thursday")
+//         break;
+//     case 5:
+//         console.log("friday")
+//         break;
+//     case 6:
+//         console.log("saturday")
+//         break;
+
+//     default:
+//         console.log("sunday")
+//         break;
+// }
+for (let i = 1; i <= 10; i++) {
+    console.log(19 * i)
+}
